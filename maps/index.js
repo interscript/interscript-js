dbd980dd0e2e53b329a9ed0accf88cc54dea10f1
webdev778 <webdev778@gmail.com>
2020-09-14 15:27:27 +0800

    refactor folder structure

---
+++ maps/index.js
@@ -0,0 +1,13 @@
+
+module.exports = (() => require('fs')
+    .readdirSync(__dirname)
+    .filter(fileName => !!/.json$/ig.test(fileName))
+    .map(fileName => fileName.split('.')[0])
+    .reduce((defaultExportObj, nextFileName) => {
+        try {
+            return {
+                ...defaultExportObj,
+                [nextFileName]: JSON.stringify(require(__dirname + `/${nextFileName}`)),
+            };
+        }catch(err) { throw err; }
+    }, {}))();
